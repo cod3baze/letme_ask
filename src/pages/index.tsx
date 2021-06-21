cebfc7dd7216e9268335b2ff4ad32f8b34d1edfa
eliasall3x <elliasallex@gmail.com>
2021-06-20 21:02:27 -0300

    INITIAL COMMIT

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>elias alexandre</h1>
+    </div>
+  );
+}
